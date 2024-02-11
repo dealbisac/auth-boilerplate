27b08fdcfc453eb2055b8ad6399f1c20ca6c24b5
dealbisac <chand.dipendra19@gmail.com>
2024-02-11 20:22:49 +0100

    add signup form

---
+++ components/auth/signup-form.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <div>
+            <h1>Signup</h1>
+
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    )
+}
+
+export default Signup
