b7bf09605637df4b1907b77713e7ce63df9d14dd
dealbisac <chand.dipendra19@gmail.com>
2024-02-18 20:03:39 +0100

    typo: fix improper import name

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -2,7 +2,7 @@
 
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
-import { getVerificationTokenByToken } from "@/data/verificiation-token";
+import { getVerificationTokenByToken } from "@/data/verification-token";
 
 export const newVerification = async (token: string) => {
     const existingToken = await getVerificationTokenByToken(token);
